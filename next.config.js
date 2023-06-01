c94446ab50b0a2cf5c4db3b63843dbb0f3a1db22
Wesley ladd <wesladd@traingrc.com>
2023-06-01 16:46:22 -0500

    added index page env variables for students to discover and abuse

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    env: {
+      COGNITO_USER_POOL_ID: process.env.COGNITO_USER_POOL_ID,
+      COGNITO_USER_POOL_CLIENT_ID: process.env.COGNITO_USER_POOL_CLIENT_ID,
+    },
+  };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,30 @@
-// pages/index.tsx
 import { NextPage } from 'next'
 
-const HomePage: NextPage = () => {
+interface HomePageProps {
+  userPoolId: string;
+  userPoolClientId: string;
+}
+
+const HomePage: NextPage<HomePageProps> = ({ userPoolId, userPoolClientId }) => {
+  // Access the userPoolId and userPoolClientId in your component
+
   return (
     <div className="container">
       <h1 className="title">Welcome to Best Cloud Company Dev Portal!</h1>
       <p className="description">This is the developer portal for our customer development teams, providing detailed documentation, examples, API reference, and more.</p>
     </div>
-  )
+  );
 }
 
-export default HomePage
+HomePage.getInitialProps = async () => {
+  const userPoolId = process.env.COGNITO_USER_POOL_ID;
+  const userPoolClientId = process.env.COGNITO_USER_POOL_CLIENT_ID;
+
+  return {
+    userPoolId: userPoolId || '',
+    userPoolClientId: userPoolClientId || '',
+  };
+};
+
+
+export default HomePage;
